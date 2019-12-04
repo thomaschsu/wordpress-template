69f090b96bd2da0f9e67a50d035f0d2698178de0
Tom Hsu <thomaschsu@gmail.com>
2019-12-04 14:41:33 -0500

    Added gulp

---
+++ settings.js
@@ -0,0 +1,14 @@
+// exports.themeLocation = './app/wp-content/themes/wordpress-template/';
+// exports.urlToPreview = 'http://wordpress-template.test';
+
+// If you're using Local by Flywheel you will
+// want your settings to be similar to the examples below:
+
+exports.themeLocation = './app/public/wp-content/themes/wordpresstemplate/';
+exports.urlToPreview = 'http://wordpress-template.local/';
+
+// Simply remove the two slashes at the front of those lines
+// to uncomment them and then delete lines #1 and #2.
+
+// Be SURE to update urlToPreview to YOUR domain and not mine.
+// Be SURE to update themeLocation to YOUR theme folder name
