b1eaecd0130958005b5994e5f5eb92b2979dffc4
Tom Hsu <thomaschsu@gmail.com>
2019-12-12 21:05:55 -0500

    Added campuses

--- app/public/wp-content/themes/wordpresstemplate/functions.php
+++ app/public/wp-content/themes/wordpresstemplate/functions.php
@@ -71,4 +71,11 @@ function university_adjust_queries($query) {
 
 add_action('pre_get_posts', 'university_adjust_queries');
 
+function my_acf_google_map_api($api)
+{
+    $api['key'] = 'X';
+    return $api;
+}
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
 ?>
