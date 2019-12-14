698509f17c674b1c9ab4df4e6285aac7f0b9ea81
Tom Hsu <thomaschsu@gmail.com>
2019-12-13 21:52:50 -0500

    Added JS for search

--- app/public/wp-content/themes/wordpresstemplate/js/modules/Search.js
+++ app/public/wp-content/themes/wordpresstemplate/js/modules/Search.js
@@ -1,6 +1,29 @@
+document.querySelector('.js-search-trigger').addEventListener('click', () => {
+    console.log(`OPENED!!!`)
+})
+
 class Search {
+    // 1. describe and create/initiate our object
     constructor() {
-        alert('Hello I am a search!');
+        this.openButton = document.querySelector('.js-search-trigger');
+        this.closeButton = document.querySelector('.search-overlay__close');
+        this.searchOverlay = document.querySelector('.search-overlay');
+        this.events();
+    }
+
+    // 2. events
+    events() {
+        this.openButton.addEventListener('click', this.openOverlay.bind(this));
+        this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
+    }
+
+    // 3. methods (function, action...)
+    openOverlay() {
+        console.log(`Opened!`)
+    }
+
+    closeOverlay() {
+        this.searchOverlay.classList.remove('search-overlay--active');
     }
 }
 
--- app/public/wp-content/themes/wordpresstemplate/js/scripts.js
+++ app/public/wp-content/themes/wordpresstemplate/js/scripts.js
@@ -1,5 +1,5 @@
 // 3rd party packages from NPM
-import $ from 'jquery';
+// import $ from 'jquery';
 import slick from 'slick-carousel';
 
 // Our modules / classes
