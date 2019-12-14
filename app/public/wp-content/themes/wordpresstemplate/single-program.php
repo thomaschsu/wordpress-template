1f14c62e07261272e4d0a53f2af867e5f56c844f
Tom Hsu <thomaschsu@gmail.com>
2019-12-13 20:26:18 -0500

    Finished campuses

--- app/public/wp-content/themes/wordpresstemplate/single-program.php
+++ app/public/wp-content/themes/wordpresstemplate/single-program.php
@@ -79,7 +79,18 @@ while (have_posts()) {
                     get_template_part('template-parts/content-event');
                 }
             }
-
+            wp_reset_postdata();
+            $relatedCampuses = get_field('related_campus');
+            echo '<hr class="section-break">';
+            if ($relatedCampuses) {
+                echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available at These Campuses:</h2>';
+                
+                echo '<ul class="min-list link-list">';
+                foreach($relatedCampuses as $campus) {
+                    ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus) ?> </a></li> <?php
+                }
+                echo '</ul>';
+            }
             ?>
     </div>
 <?php }
