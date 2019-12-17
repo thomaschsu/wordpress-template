617fb81aae59a767da2c2fcda6f3ac6c9c901e75
Tom Hsu <thomaschsu@gmail.com>
2019-12-17 17:40:30 -0500

    Added keyboard event

--- app/public/wp-content/themes/wordpresstemplate/js/modules/Search.js
+++ app/public/wp-content/themes/wordpresstemplate/js/modules/Search.js
@@ -5,21 +5,36 @@ class Search {
         this.closeButton = document.querySelector('.search-overlay__close');
         this.searchOverlay = document.querySelector('.search-overlay');
         this.events();
+        this.isOverlayOpen = false;
     }
 
     // 2. events
     events() {
         this.openButton.addEventListener('click', this.openOverlay.bind(this));
         this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
+        document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
     }
 
     // 3. methods (function, action...)
     openOverlay() {
         this.searchOverlay.classList.add('search-overlay--active');
+        document.querySelector('body').classList.add('body-no-scroll');
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.classList.remove('search-overlay--active');
+        document.querySelector('body').classList.remove('body-no-scroll');
+        this.isOverlayOpen = false;
+    }
+
+    keyPressDispatcher(e) {
+        if (e.keyCode === 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+        if (e.keyCode === 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
     }
 }
 
