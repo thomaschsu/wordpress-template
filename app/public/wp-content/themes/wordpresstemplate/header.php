ff2ef72bce505a7e88ba420e8f95e17f1d91fbf3
Tom Hsu <thomaschsu@gmail.com>
2019-12-17 17:14:00 -0500

    Modified header php

--- app/public/wp-content/themes/wordpresstemplate/header.php
+++ app/public/wp-content/themes/wordpresstemplate/header.php
@@ -17,16 +17,16 @@
                 <nav class="main-navigation">
                     <ul>
                         <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 17) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                        <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"'?>><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
-                        <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"'?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
-                        <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"'?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
-                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
+                        <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
+                        <li <?php if (get_post_type() == 'event' or is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
+                        <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
+                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
                     </ul>
                 </nav>
                 <div class="site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
                     <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
-                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                    <span class="js-search-trigger search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
                 </div>
             </div>
         </div>
